23e66350da26f132b2085043c25e5dae829d7d15
arthurfirmino <arthur.p.v.firmino@gmail.com>
2018-02-21 11:08:14 -0800

    AA Lab changes

--- antialiasing/main.cpp
+++ antialiasing/main.cpp
@@ -1,80 +1,49 @@
 #include <OpenGP/GL/Application.h>
-#include "OpenGP/GL/glfw_helpers.h"
+
+const char *vshader =
+#include "vshader.glsl"
+;
+const char *fshader =
+#include "fshader.glsl"
+;
+
 using namespace OpenGP;
 
 constexpr int width = 600;
 constexpr int height = 600;
 
-std::unique_ptr<Shader> SSAAshader;
-std::unique_ptr<Shader> MSAAshader;
-
-const char *ssaa_vshader =
-#include "ssaa_vshader.glsl"
-;
-const char *ssaa_fshader =
-#include "ssaa_fshader.glsl"
-;
-const char *msaa_vshader =
-#include "msaa_vshader.glsl"
-;
-const char *msaa_fshader =
-#include "msaa_fshader.glsl"
-;
-
+std::unique_ptr<Shader> shader;
 std::unique_ptr<GPUMesh> fsquad;
-std::unique_ptr<GPUMesh> triangle;
 
 void init();
 
 int main() {
+
     Application app;
     init();
 
-    glfwWindowHint(GLFW_SAMPLES, 0);
-    Window& windowSSAA = app.create_window([](Window&){
+    Window& window = app.create_window([](Window&){
         glViewport(0, 0, width, height);
         glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        SSAAshader->bind();
-        fsquad->set_attributes(*SSAAshader);
+
+        shader->bind();
+        fsquad->set_attributes(*shader);
         fsquad->draw();
-        SSAAshader->unbind();
+        shader->unbind();
     });
-    windowSSAA.set_title("Super Sampling AA Example");
-    windowSSAA.set_size(width, height);
+    window.set_title("Super Sampling AA Example");
+    window.set_size(width, height);
 
-    //glfwWindowHint(GLFW_SAMPLES, 16);
-    Window& windowMSAA = app.create_window([](Window&){
-        //glEnable(GL_MULTISAMPLE);
-        glViewport(0, 0, width, height);
-        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        MSAAshader->bind();
-        triangle->set_attributes(*MSAAshader);
-        triangle->draw();
-        MSAAshader->unbind();
-        glDisable(GL_MULTISAMPLE);
-    });
-    windowMSAA.set_title("Multi Sampling AA Example");
-    windowMSAA.set_size(width, height);
-
-    //std::cout << "\nRunning Application" << std::endl;
     return app.run();
 }
 
 void init() {
 
-    SSAAshader = std::unique_ptr<Shader>(new Shader());
-    //SSAAshader->verbose = true;
-    SSAAshader->add_vshader_from_source(ssaa_vshader);
-    SSAAshader->add_fshader_from_source(ssaa_fshader);
-    SSAAshader->link();
-
-    MSAAshader = std::unique_ptr<Shader>(new Shader());
-    //MSAAshader->verbose = true;
-    MSAAshader->add_vshader_from_source(msaa_vshader);
-    MSAAshader->add_fshader_from_source(msaa_fshader);
-    MSAAshader->link();
+    shader = std::unique_ptr<Shader>(new Shader());
+    shader->add_vshader_from_source(vshader);
+    shader->add_fshader_from_source(fshader);
+    shader->link();
 
     fsquad = std::unique_ptr<GPUMesh>(new GPUMesh());
     std::vector<Vec3> fsquad_vposition = {
@@ -95,16 +64,4 @@ void init() {
         Vec2( 1,  1)
     };
     fsquad->set_vtexcoord(fsquad_vtexcoord);
-
-    triangle = std::unique_ptr<GPUMesh>(new GPUMesh());
-    std::vector<Vec3> triangle_vposition = {
-        Vec3(-0.5f,     0, 0),
-        Vec3( 0.3f, -0.3f, 0),
-        Vec3( 0.5f,  0.3f, 0)
-    };
-    triangle->set_vbo<Vec3>("vposition", triangle_vposition);
-    std::vector<unsigned int> triangle_indices = {
-        0, 1, 2
-    };
-    triangle->set_triangles(triangle_indices);
 }
--- antialiasing/msaa_vshader.glsl
+++
@@ -1,9 +0,0 @@
-R"(
-#version 330 core
-in vec3 vposition;
-in vec2 vtexcoord;
-
-void main() {
-    gl_Position = vec4(vposition, 1.0);
-}
-)"
